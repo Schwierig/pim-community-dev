189a39d05d152656bdeefaa5a9c23c39926eb721
Samir Boulil <samir.boulil@akeneo.com>
2016-10-11 19:17:50 +0200

    Fix Category normalizer in versioning and fixed some specs

--- src/Pim/Bundle/VersioningBundle/Normalizer/Flat/CategoryNormalizer.php
+++ src/Pim/Bundle/VersioningBundle/Normalizer/Flat/CategoryNormalizer.php
@@ -2,19 +2,65 @@
 
 namespace Pim\Bundle\VersioningBundle\Normalizer\Flat;
 
-use Pim\Component\Catalog\Normalizer\Structured\CategoryNormalizer as BaseNormalizer;
+use Pim\Component\Catalog\Model\CategoryInterface;
+use Pim\Component\Catalog\Normalizer\Standard\CategoryNormalizer as StandardNormalizer;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 
 /**
  * A normalizer to transform a category entity into a flat array
- *
- * TODO (2014-07-25 11:30 by Gildas): Couldn't the Structured CategoryNormalizer class could be used here too?
  *
  * @author    Filips Alpe <filips@akeneo.com>
  * @copyright 2013 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-class CategoryNormalizer extends BaseNormalizer
+class CategoryNormalizer implements NormalizerInterface
 {
     /**  @var string[] */
-    protected $supportedFormats = ['csv'];
+    protected $supportedFormats = ['flat'];
+
+    /** @var TranslationNormalizer */
+    protected $translationNormalizer;
+
+    /** @var CategoryNormalizer */
+    protected $standardNormalizer;
+
+    /**
+     * @param StandardNormalizer    $standardNormalizer
+     * @param TranslationNormalizer $translationNormalizer
+     */
+    public function __construct(
+        StandardNormalizer $standardNormalizer,
+        TranslationNormalizer $translationNormalizer
+    ) {
+        $this->standardNormalizer = $standardNormalizer;
+        $this->translationNormalizer = $translationNormalizer;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function normalize($object, $format = null, array $context = [])
+    {
+        if (!$this->standardNormalizer->supportsNormalization($object, 'standard')) {
+            return null;
+        }
+
+        $standardCategory = $this->standardNormalizer->normalize($object, 'standard', $context);
+        $flatCategory = $standardCategory;
+
+        unset($flatCategory['labels']);
+        if ($this->translationNormalizer->supportsNormalization($standardCategory['labels'], 'flat')) {
+            $flatCategory += $this->translationNormalizer->normalize($standardCategory['labels'], 'flat', $context);
+        }
+
+        return $flatCategory;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function supportsNormalization($data, $format = null)
+    {
+        return $data instanceof CategoryInterface && in_array($format, $this->supportedFormats);
+    }
 }
