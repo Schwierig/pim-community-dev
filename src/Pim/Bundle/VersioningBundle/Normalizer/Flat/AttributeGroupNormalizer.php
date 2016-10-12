3762eaa54b5e92a71f2e8a7eac8f1e3194fec19f
Samir Boulil <samir.boulil@akeneo.com>
2016-10-12 16:02:00 +0200

    Fix AttributeGroupNormalizer for versionning

--- src/Pim/Bundle/VersioningBundle/Normalizer/Flat/AttributeGroupNormalizer.php
+++ src/Pim/Bundle/VersioningBundle/Normalizer/Flat/AttributeGroupNormalizer.php
@@ -2,7 +2,10 @@
 
 namespace Pim\Bundle\VersioningBundle\Normalizer\Flat;
 
-use Pim\Component\Catalog\Normalizer\Structured\AttributeGroupNormalizer as BaseNormalizer;
+use Pim\Bundle\CatalogBundle\Entity\AttributeGroup;
+use Pim\Component\Catalog\Model\AttributeGroupInterface;
+use Pim\Component\Catalog\Normalizer\Standard\AttributeGroupNormalizer as StandardNormalizer;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 
 /**
  * Flat attribute group normalizer
@@ -11,21 +14,62 @@ use Pim\Component\Catalog\Normalizer\Structured\AttributeGroupNormalizer as Base
  * @copyright 2013 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-class AttributeGroupNormalizer extends BaseNormalizer
+class AttributeGroupNormalizer implements NormalizerInterface
 {
+    const ITEM_SEPARATOR = ',';
+
     /** @var string[] */
-    protected $supportedFormats = ['csv', 'flat'];
+    protected $supportedFormats = ['flat'];
+
+    /** @var TranslationNormalizer */
+    protected $translationNormalizer;
+
+    /** @var CategoryNormalizer */
+    protected $standardNormalizer;
+
+    /**
+     * @param StandardNormalizer    $standardNormalizer
+     * @param TranslationNormalizer $translationNormalizer
+     */
+    public function __construct(
+        StandardNormalizer $standardNormalizer,
+        TranslationNormalizer $translationNormalizer
+    ) {
+        $this->standardNormalizer = $standardNormalizer;
+        $this->translationNormalizer = $translationNormalizer;
+    }
 
     /**
      * {@inheritdoc}
      */
     public function normalize($object, $format = null, array $context = [])
     {
-        $data = parent::normalize($object, $format, $context);
+        if (!$this->standardNormalizer->supportsNormalization($object, 'standard')) {
+            return null;
+        }
 
-        asort($data['attributes']);
-        $data['attributes'] = implode(',', $data['attributes']);
+        $standardAttributeGroup = $this->standardNormalizer->normalize($object, 'standard', $context);
+        $flatAttributeGroup = $standardAttributeGroup;
 
-        return $data;
+        $flatAttributeGroup['attributes'] = implode(self::ITEM_SEPARATOR, $standardAttributeGroup['attributes']);
+
+        unset($flatAttributeGroup['labels']);
+        if ($this->translationNormalizer->supportsNormalization($standardAttributeGroup['labels'], 'flat')) {
+            $flatAttributeGroup += $this->translationNormalizer->normalize(
+                $standardAttributeGroup['labels'],
+                'flat',
+                $context
+            );
+        }
+
+        return $flatAttributeGroup;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function supportsNormalization($data, $format = null)
+    {
+        return $data instanceof AttributeGroupInterface && in_array($format, $this->supportedFormats);
     }
 }
